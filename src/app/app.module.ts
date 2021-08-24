7e0dd49faaf2572c7464dafb730d5803e800c8ad
laisassis <laisflor1003@gmail.com>
2021-08-24 12:32:05 -0300

    atualização menu e rodapé

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntrarComponent,
+    CadastrarComponent,
+    RodapeComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
